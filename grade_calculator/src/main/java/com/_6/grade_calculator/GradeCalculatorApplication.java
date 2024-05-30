01b1569f711c4de22c1e2714cdd33cce04b067ba
martinzukowski <martinzukowski04@gmail.com>
2024-05-30 14:41:45 -0700

    add dockerfile

--- grade_calculator/src/main/java/com/_6/grade_calculator/GradeCalculatorApplication.java
+++ grade_calculator/src/main/java/com/_6/grade_calculator/GradeCalculatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradeCalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GradeCalculatorApplication.class, args);
+		SpringApplication.run(primarySource:GradeCalculatorApplication.class, args);
 	}
 
 }
