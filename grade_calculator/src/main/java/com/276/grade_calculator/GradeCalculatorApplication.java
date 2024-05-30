8b8f151129e72f65799f37f18b21e0ba849893ec
martinzukowski <martinzukowski04@gmail.com>
2024-05-29 20:57:45 -0700

    spring

---
+++ grade_calculator/src/main/java/com/276/grade_calculator/GradeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com._6.grade_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCalculatorApplication.class, args);
+	}
+
+}
---
+++ grade_calculator/src/test/java/com/_6/grade_calculator/GradeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.grade_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
